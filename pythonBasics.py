ba4fad4566a9c8b8f985bb2ec72c1786b26fd5c1
Andrii X <khrinenko.andrew@gmail.com>
2024-02-15 21:01:09 +0200

    switch statement (match)

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,15 +1,13 @@
-print("Hello. Input bill_total:")
-bill_total = float(input())
+print("Hello. Input http status:")
+stat = float(input())
 
-discount1 = 10
-discount2 = 20
+match stat:
+    case 200 | 201:
+        print("successs")
+    case 400:
+        print("bad")
+    case 500:
+        print("error")
+    case _:
+        print("unknown")
 
-if 100 < bill_total <= 200:
-    print("greater than 100!")
-    bill_total = bill_total - discount1
-elif bill_total > 200:
-    print("greater than 200!")
-else:
-    print("less than 100")
-
-print("total is " + str(bill_total))
