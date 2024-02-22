f931fd58ae98fa3f8e41c17d513108566066a2bd
Andrii <khrinenko.andrew@gmail.com>
2024-02-22 16:43:33 +0200

    basic data structures

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,29 +1,25 @@
-# Example: Search for a specific number in a list and
-# stop the search once it's found.
-numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
-target = 6
-print("Example for BREAK statement ->")
-for idx, number in enumerate(numbers):
-    if number == target:
-        print(f"Found the target number: {target} on {idx+1}-th position")
-        break  # Exit the loop once the target is found
+# Lists
+list1 = [1, 2, 3, 4, 5]
 
-#----------------------------------
+print(list1, sep=" ")
 
-# Example: Print only odd numbers from a list, skipping even numbers.
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Example for CONTINUE statement ->")
-for number in numbers:
-    if number % 2 == 0:  # Check if the number is even
-        continue  # Skip to the next
-    print(number)  # This will only execute for odd numbers
+list1.append(6)
+list1.insert(0, -1)
 
-#----------------------------------
+print(list1, sep=" ")
 
-# Example: A loop that checks for a specific condition but doesn't require action yet.
-numbers = [1, 2, 3, 4, 5]
-print("Example for PASS statement ->")
-for number in numbers:
-    if number == 3:
-        pass  # Placeholder for future code.
-    print(number)
+# Tuples
+my_tuple = (1, 2, 3, 4, 5)
+print(type(my_tuple))
+
+# Sets
+my_set = {1, 2, 3, 4, 5}
+second_set = {4, 5, 6, 7}
+
+print(my_set.intersection(second_set))
+
+# Dictionaries
+my_dict = {1: "coffee", 2: "tea", 3: "juice"}
+
+for key, value in my_dict.items():
+    print(f"{key} -> {value}")
