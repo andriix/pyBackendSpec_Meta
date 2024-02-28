216665bcb2fbbb564f18c94bba926230870df437
Andrii <khrinenko.andrew@gmail.com>
2024-02-28 16:54:43 +0200

    basic file handling

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,26 +1,16 @@
-# Starter code
-items = [1,2,3,4,5]
+file_location_and_name = 'test_file.txt'
+
+# with open(file_location_and_name, 'w') as file:
+#    file.writelines(["\n First line!", "\n Second line!"])
+
+with open(file_location_and_name, 'a') as file:
+    file.writelines(["\n First line2!", "\n Second line2!"])
+
 try:
-    item = items[6]
-    print(item)
-except IndexError as e:
-    print("Item does not exist in the list")
-
-# Starter code
-def divide_by(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return 0
-    except Exception as exc:
-        print(exc, 'Something went wrong!')
-
-ans = divide_by(40, 0)
-print(ans)
-
-# Starter code
-try:
-    with open('file_does_not_exist.txt', 'r') as file:
-        print(file.read())
-except FileNotFoundError as e:
-    print("The file could not be found")
+    with open(file_location_and_name, 'r') as file:
+        content = file.read()
+    print("test.file.txt contains ->", content)
+except FileNotFoundError:
+    print("The file was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
