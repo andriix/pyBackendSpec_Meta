cf3cef1139c4792cdf8a649355bdc2ebd76e32cd
Andrii <khrinenko.andrew@gmail.com>
2024-03-04 20:05:09 +0200

    example of work with a file

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,15 +1,21 @@
-file_location_and_name = 'test_file.txt'
+import os
 
-# with open(file_location_and_name, 'w') as file:
-#    file.writelines(["\n First line!", "\n Second line!"])
+current_directory = os.getcwd()
 
-with open(file_location_and_name, 'a') as file:
-    file.writelines(["\n First line2!", "\n Second line2!"])
+my_file_name = input("What file do I need to open:")
+my_file_dir_and_name = current_directory+"/"+my_file_name
+
+# with open(my_file_dir_and_name, 'w') as file:
+#     file.writelines(["\n First line!", "\n Second line!"])
+# with open(file_location_and_name, 'a') as file:
+#    file.writelines(["\n First line2!", "\n Second line2!"])
 
 try:
-    with open(file_location_and_name, 'r') as file:
-        content = file.read()
-    print("test.file.txt contains ->", content)
+    print("my test txt file contains ->\n")
+    with open(my_file_dir_and_name, 'r', encoding='utf-8') as my_file:
+        content = my_file.readlines()
+        for line in content:
+            print(line)
 except FileNotFoundError:
     print("The file was not found.")
 except Exception as e:
