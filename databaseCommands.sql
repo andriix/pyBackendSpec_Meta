945845d007444f46cfeb07155951e5dc8fa24b24
Andrii <khrinenko.andrew@gmail.com>
2024-04-03 16:45:19 +0300

    sql: minor change to sql file

--- databaseCommands.sql
+++ databaseCommands.sql
@@ -1,11 +1,14 @@
 -- MySQL terminal commands ->
-
+mysql;
 -- LAB 1
 create database cm_devices;
 
 use cm_devices;
 
 CREATE TABLE devices (deviceID int, deviceName varchar(50), price decimal);
+
+CREATE TABLE feedback(feedbackID CHAR(8), feedbackType VARCHAR(100),
+                      comment TEXT(500));
 
 show columns from devices;
 --------------------
