7ea33deb546c0ef8a455a1cef839096b2bb05576
Andrii <khrinenko.andrew@gmail.com>
2024-04-03 09:23:03 +0300

    sql commands

---
+++ databaseCommands.sql
@@ -0,0 +1,12 @@
+-- MySQL terminal commands ->
+
+-- LAB 1
+create database cm_devices;
+
+use cm_devices;
+
+CREATE TABLE devices (deviceID int, deviceName varchar(50), price decimal);
+
+show columns from devices;
+--------------------
+
