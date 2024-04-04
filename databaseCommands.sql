f3dba6d27b9214ea69e9c4c2bf64273b0df9a454
Andrii <khrinenko.andrew@gmail.com>
2024-04-04 09:55:42 +0300

    sql: ALTER statements

--- databaseCommands.sql
+++ databaseCommands.sql
@@ -4,12 +4,21 @@ mysql;
 create database cm_devices;
 
 use cm_devices;
-
+-- create statements
 CREATE TABLE devices (deviceID int, deviceName varchar(50), price decimal);
 
 CREATE TABLE feedback(feedbackID CHAR(8), feedbackType VARCHAR(100),
                       comment TEXT(500));
+CREATE TABLE Address (id int NOT NULL,  street VARCHAR(255),
+                      postcode VARCHAR(10) DEFAULT "HA97DE",
+                      town VARCHAR(30) DEFAULT "Harrow");
 
 show columns from devices;
+-- alter statements
+ALTER TABLE devices ADD (deviceType varchar(50), deviceSize varchar(10));
+
+ALTER TABLE devices DROP COLUMN deviceType;
+
+ALTER TABLE devices MODIFY deviceName varchar(100);
 --------------------
 
