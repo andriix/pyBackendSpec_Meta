249a166d2d225ac1ddf50878b38a80be625155a7
Andrii X <khrinenko.andrew@gmail.com>
2026-03-22 21:35:48 +0200

    continue fastapi vid

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 BALANCE : dict = {}
+
+class OperationRequest(BaseModel):
+    wallet_name: str
+    amount: float
+    description: str | None = None
 
 @app.get("/balance")
 def get_balance(wallet_name: str | None = None):
@@ -14,3 +20,13 @@ def get_balance(wallet_name: str | None = None):
             detail=f"Wallet '{wallet_name}' not found"
             )
     return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+@app.post("/wallets/{name}")
+def create_wallet(name: str, initial_balance: float | None = 0):
+    if name in BALANCE:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Wallet '{name}' already exists"
+            )
+    BALANCE[name] = initial_balance
+    return {"message": f"Wallet '{name}' created successfully"}
